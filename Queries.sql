f79ed41f41a13646579ecaeacf7a4922dd5cc9f3
Janelle Zorvan <jzorvan@gmail.com>
2017-02-27 11:45:27 -0600

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,4 +1,3 @@
-
 -- 1. Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.
 
 SELECT CustomerId AS 'Customer Id#', Last Name || ', ' || FirstName AS 'Customer', Country AS 'Customer Country' FROM Customer WHERE Country != 'USA';
